2084a299f728fd4c5e7c744037465ace7dc29c11
Aditi Khandelwal <46483537+AditiKhandelwal@users.noreply.github.com>
2020-09-10 15:34:50 +0530

    Update peterson.c

--- peterson.c
+++ peterson.c
@@ -5,31 +5,32 @@ int turn;
 int flag[2];
 int main(void)
 {
-        
+    
         int pid =fork();
-        int t =10;
-        while(t--)
-        {
-                if(pid==0)
+        
+              if(pid==0)
                 {
-                     
+                    int t=100;
+                     while(t--){
                      flag[0]=1;
                      turn=1;
                      while(flag[1]==1&&turn==1);
                      printf("This is critical section:child process\n");
                      flag[0]=0;
+                     }
                 }
                 else
                 {
-                    
-                       
+                       int t=100;
+                       while(t--){
                         flag[1]=1;
                         turn=0;
                         while(flag[0]&&turn==0);
                         printf("This is critical section:parent process  \n");
                         flag[1]=0;
+                       }
                 }
-        }
+        
         return 0;
 
 }
